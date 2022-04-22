56d58cf6314321babb1417be1356b0de1faba77c
Henno Täht <henno.taht@gmail.com>
2022-04-22 20:03:22 +0300

    wip

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+	import {_} from 'svelte-i18n'
 </script>
 
 <svelte:head>
@@ -11,22 +11,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	{$_('SelectEmployee')}
 </section>
 
 <style>
@@ -38,9 +23,7 @@
 		flex: 1;
 	}
 
-	h1 {
-		width: 100%;
-	}
+
 
 	.welcome {
 		position: relative;
